40a714c3e3023fe3bb385e8d84b624dc2e854e07
Noah Melngailis <noah.melngailis@gmail.com>
2020-08-31 21:48:44 -0500

    os path check

--- wrangle_xml.py
+++ wrangle_xml.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os.path
 
 import xml.etree.ElementTree as etree
 
@@ -19,3 +20,52 @@ def wrangle_xml():
     df = build_df(df)
     
     return df
+
+def validate_data_sets():
+    df = pd.DataFrame(root[4].attrib, index=[0])
+    
+    print(root[5].attrib['type'])
+    
+    ## check to see if attribute is step counter
+    i = 4
+    while i < 67920:
+        if root[i].attrib['type'] == 'HKQuantityTypeIdentifierStepCount':
+        else:
+            print(i)
+            break
+            i += 1
+            
+    root[67919].attrib
+    
+    i = 67919
+    while i < 1_000_000:
+        if root[i].attrib['type'] == 'HKQuantityTypeIdentifierDistanceWalkingRunning':
+        else:
+            print(f'{i} is false and break')
+            break
+        i += 1
+        
+    root[135827].attrib
+    
+    i = 135827
+    while i < 1_000_000:
+        if root[i].attrib['type'] == 'HKQuantityTypeIdentifierFlightsClimbed':
+        else:
+            print(f'{i} is false and break')
+            break
+        i += 1
+        
+    root[137028].attrib
+    
+    i = 
+    type_list = []
+    while i < 1_000_000:
+        if root[i].attrib['type'] in type_list:
+
+        else:
+            type_list.append(root[i].attrib['type'])
+        i +=1
+    
+    type_list
+    
+    return type_list
